28bcb6282073efdb9674ee65d0e1216f7f7b6d32
Mahmoud-Mohammed-Fathallah <83406556+Mahmoud-Mohammed-Fathallah@users.noreply.github.com>
2022-09-04 03:47:11 +0200

    Update script.js

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,12 +1,12 @@
 function display() {
-    document.querySelector(".menu").classList.toggle("invisible")
+    document.querySelector(".menu").classList.toggle("menu-show")
 }
 function move(where) {
     var arr = document.querySelectorAll(".pro-div");
     var num = 0;
+    let element;
     for (var i = 0; i < arr.length; i++) {
-        if (arr[i].classList.contains("active")) {
-            arr[i].classList.remove("active");
+        if (arr[i].classList.contains("current")) {
             if (arr[i].classList.contains("num1")) {
                 num = 1;
             } else if (arr[i].classList.contains("num2")) {
@@ -14,11 +14,18 @@ function move(where) {
             } else {
                 num = 3;
             }
+            arr[i].classList.add("leaving");
+            arr[i].classList.remove("current");
+            element = arr[i];
+            setTimeout(()=>{
+                element.classList.remove("leaving")
+            },1000)
         }
     }
     if (where == "left") {
-        console.log("in");
+        console.log(num);
         num = (num - 1) % 3;
+        console.log(num);
     } else {
         num = (num + 1) % 3;
     }
@@ -26,14 +33,8 @@ function move(where) {
         num = 3;
     }
 
-    var className = "num" + num;
-    for (var j = 0; j < arr.length; j++) {
-        if (arr[j].classList.contains(className)) {
-            arr[j].classList.add("active");
-            arr[j].classList.remove("invisible");
-        } else {
-            arr[j].classList.add("invisible");
-        }
-    }
+    var className = ".num" + num;
+    document.querySelector(className).classList.add("current");
+    
 }
-setInterval(move, 5000);
+setInterval(move,5000);
